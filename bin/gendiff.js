701163d11b25ef714f02ffc2f68200db8b31d87c
Zeal22 <fevral12@mail.ru>
2022-03-21 12:19:18 +0500

    fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,6 @@ program
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format <type>', 'output format')
-    .argument('<filepath1>')
-    .argument('<filepath2>')
+    .argument('<filepath1>, <filepath2>')
 
 program.parse(process.argv);
